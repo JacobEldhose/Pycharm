ad09cb137873975060c23feeb3f93b6322019209
Jacob Eldhose <jacobeldhose1234@gmail.com>
2024-07-18 15:19:42 +0530

    Done

--- Weekly_task/week_1/prob_4.py
+++ Weekly_task/week_1/prob_4.py
@@ -1,9 +1,26 @@
 """Write a Python program to generate all possible valid identifiers of length n,
-where n is provided by the user.
+where n is provided by the user character of n number is given by the user.
 """
 
-n_letters = int(input("Enter the limit: "))
-identifier = input(f"Enter your string with {n_letters}")
 
+def generate_words(letters):
+    results = ['']
+
+    for letter in letters:
+        new_results = []
+        for word in results:
+            for i in range(len(word) + 1):
+                new_word = word[:i] + letter + word[i:]
+                if new_word not in new_results:
+                    new_results.append(new_word)
+        results = new_results
+
+    return results
+
+
+# Example usage
+letters = 'abc'
+words = generate_words(letters)
+print(words)
 
         
