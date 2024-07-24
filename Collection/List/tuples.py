362d0e9d144f3939a9ecf9573c61b9486b67ba10
Jacob Eldhose <jacobeldhose1234@gmail.com>
2024-07-24 16:07:30 +0530

    Week_2

---
+++ Collection/List/tuples.py
@@ -0,0 +1,9 @@
+"""
+Tuple = ()
+insertion order is preserved
+"""
+tu = (10, 10, 'elsa', True, False)
+print(tu)
+
+tu = (10)
+print(tu)
---
+++ Collection/Set/demo.py
@@ -0,0 +1,6 @@
+# How to define
+# Mutable
+
+tu = {1,2,3,4}
+
+print(tu)
